fc970262fe1377ac98e4b1c816ec95ee33a6e808
unknown <danjschmidt@gmail.com>
2016-02-29 16:46:32 -0800

    post testing

--- hippo_front_1/index.js
+++ hippo_front_1/index.js
@@ -27,19 +27,41 @@ parameter.
 
 app.get('/token', function(request, response) {
     var requestRemote = require('request');
-    var identity = randomUsername();
-    requestRemote('http://hippo-back.herokuapp.com/', function (error, responseRemote, body) {
-        if (!error && responseRemote.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage. 
+    // var identity = randomUsername();
+    // requestRemote('http://hippo-back.herokuapp.com/auth/dealer', function (error, responseRemote, body) {
+    //     if (!error && responseRemote.statusCode == 200) {
+    //         console.log(body);
+    //     }
+    //     var obj = JSON.parse(body);
+    //     // Serialize the token to a JWT string and include it in a JSON response
+    //     response.send({
+    //         identity: obj.identity,
+    //         token: obj.token
+    //     });
+    // });
+
+    requestRemote.post({
+        //url: 'http://hippo-back.herokuapp.com/auth/dealer', //URL to hit
+        url: 'http://127.0.0.1:3000/auth/dealer',
+        qs: {time: 'now'}, //Query string data
+        method: 'POST',
+        //Lets post the following key/values as form
+        json: {
+            identity: 'dealer123'
         }
-        var obj = JSON.parse(body);
-        // Serialize the token to a JWT string and include it in a JSON response
-        response.send({
-            identity: obj.identity,
-            token: obj.token
-        });
+    }, function(error, innerResponse, body) {
+        if(error) {
+            console.log(error);
+        } else {
+            //var obj = JSON.parse(body);
+            // Serialize the token to a JWT string and include it in a JSON response
+            response.send({
+                identity: body.identity,
+                token: body.token
+            });
+        }
     });
-    
+
 });
 
 // Create http server and run it.
